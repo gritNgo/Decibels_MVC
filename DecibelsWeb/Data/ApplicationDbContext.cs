559ce93ea311b4afcca3f916eb7aded774580cdc
gritNgo <dustspeck00@gmail.com>
2024-06-29 11:29:36 +0200

    Setup ApplicationDbContext

--- DecibelsWeb/Data/ApplicationDbContext.cs
+++ DecibelsWeb/Data/ApplicationDbContext.cs
@@ -4,6 +4,10 @@ namespace DecibelsWeb.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        
+        // Configuration for registering ApplicationDbContext to DbContext using options
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
     }
 }
