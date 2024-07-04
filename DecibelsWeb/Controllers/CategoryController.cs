dba1e4b67b76260aa20710352dbd0a8228d7f3fc
gritNgo <dustspeck00@gmail.com>
2024-07-04 06:31:21 +0200

    Create Category Controller

---
+++ DecibelsWeb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DecibelsWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
