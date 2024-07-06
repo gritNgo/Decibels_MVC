067ba97edef35a6145660038e9455896c09d38d2
gritNgo <dustspeck00@gmail.com>
2024-07-06 09:03:06 +0200

    Input tag helpers

--- DecibelsWeb/Models/Category.cs
+++ DecibelsWeb/Models/Category.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DecibelsWeb.Models
 {
     public class Category
     {
+        // Key & Required are data annotations required for EF Core
         [Key]
         public int Id { get; set; }
         [Required]
+        // DisplayName is a data annotation useful for client-side UI/validation
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
