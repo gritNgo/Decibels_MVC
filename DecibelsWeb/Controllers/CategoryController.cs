70a9e13918016c86b1a09adef61b4ecdb8533f6b
gritNgo <dustspeck00@gmail.com>
2024-07-07 12:01:31 +0200

    Get Category Details to Edit

--- DecibelsWeb/Controllers/CategoryController.cs
+++ DecibelsWeb/Controllers/CategoryController.cs
@@ -26,9 +26,48 @@ namespace DecibelsWeb.Controllers
             return View();
         }
 
-        // This annotation identifies an action that supports the HTTP POST method from the Create Category form
+        // This annotation identifies an action that supports the HTTP POST method from the Create Category form (When this is absent it's always a GET)
         [HttpPost]
         public IActionResult Create(Category obj) // an object that takes the Category Model properties of the form will be created
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display Order cannot be the same as Category Name");
+            }
+
+            if (ModelState.IsValid) // by checking obj against the Category Model and it's validations
+            {
+                _db.Categories.Add(obj); // Add is an Entity Framework method that tracks the given entity and any changes to be made in the database
+                _db.SaveChanges();  // creates the Category on the database
+                // Redirects to the Index view which is reloaded once Category is added
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound(); // or return the Error View
+            }
+
+            Category? categoryFromDb = _db.Categories.Find(id); // Retrieves entity that's primary key
+            /* Other ways to retrieve a record
+             * Category? categoryFromDb1 = _db.Categories.FirstOrDefault(category => category.Id==id);
+             * Category? categoryFromDb2 = _db.Categories.Where(category => category.Id == id).FirstOrDefault();
+            */
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        // This annotation identifies an action that supports the HTTP POST method from the Create Category form
+        [HttpPost]
+        public IActionResult Edit(Category obj) // an object that takes the Category Model properties of the form will be created
         {
             if (obj.Name == obj.DisplayOrder.ToString())
             {
