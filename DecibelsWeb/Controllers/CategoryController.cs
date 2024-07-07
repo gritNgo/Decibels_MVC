75d02a51d46ad3b532aa6e7b57ca68c5323fc317
gritNgo <dustspeck00@gmail.com>
2024-07-07 07:53:23 +0200

    Create Category

--- DecibelsWeb/Controllers/CategoryController.cs
+++ DecibelsWeb/Controllers/CategoryController.cs
@@ -19,11 +19,21 @@ namespace DecibelsWeb.Controllers
             return View(objCategoryList);
         }
 
-        // When creating a new page, first create an action method
-        // that will be invoked on the controller. Then the View
+        // When creating a new from an existing page, first create an action method
+        // that will be invoked on the controller. Then the View.
         public IActionResult Create()
         {
             return View();
+        }
+
+        // This annotation identifies an action that supports the HTTP POST method from the Create Category form
+        [HttpPost]
+        public IActionResult Create(Category obj) // an object that takes the Category Model properties of the form will be created
+        {
+            _db.Categories.Add(obj); // Add is an Entity Framework method that tracks the given entity and any changes to be made in the database
+            _db.SaveChanges();  // creates the Category on the database
+            // Redirects to the Index view which is reloaded once Category is added
+            return RedirectToAction("Index", "Category");
         }
     }
 }
