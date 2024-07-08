262dce19e6795c5b25d2882fda9af047fca8c48f
gritNgo <dustspeck00@gmail.com>
2024-07-08 10:44:42 +0200

    Update Category

--- DecibelsWeb/Controllers/CategoryController.cs
+++ DecibelsWeb/Controllers/CategoryController.cs
@@ -49,7 +49,7 @@ namespace DecibelsWeb.Controllers
         {
             if (id == null || id == 0)
             {
-                return NotFound(); // or return the Error View
+                return NotFound(); // or return an Error View
             }
 
             Category? categoryFromDb = _db.Categories.Find(id); // Retrieves entity that's primary key
@@ -65,20 +65,14 @@ namespace DecibelsWeb.Controllers
             return View(categoryFromDb);
         }
 
-        // This annotation identifies an action that supports the HTTP POST method from the Create Category form
         [HttpPost]
-        public IActionResult Edit(Category obj) // an object that takes the Category Model properties of the form will be created
-        {
-            if (obj.Name == obj.DisplayOrder.ToString())
+        public IActionResult Edit(Category obj) 
+        {            
+            if (ModelState.IsValid) 
             {
-                ModelState.AddModelError("name", "Display Order cannot be the same as Category Name");
-            }
-
-            if (ModelState.IsValid) // by checking obj against the Category Model and it's validations
-            {
-                _db.Categories.Add(obj); // Add is an Entity Framework method that tracks the given entity and any changes to be made in the database
-                _db.SaveChanges();  // creates the Category on the database
-                // Redirects to the Index view which is reloaded once Category is added
+                _db.Categories.Update(obj);
+                _db.SaveChanges();  
+                
                 return RedirectToAction("Index", "Category");
             }
             return View();
