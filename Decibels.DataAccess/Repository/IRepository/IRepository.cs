be7cbd23646efad79a14f33ba2e2d5d7fcf68d79
gritNgo <dustspeck00@gmail.com>
2024-07-10 17:01:26 +0200

    IRepository interface

---
+++ Decibels.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decibels.DataAccess.Repository.IRepository
+{
+    // A Model class (ex: Category) will implement this interface
+    internal interface IRepository<T> where T : class
+    {
+        // T- Category
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+
+    }
+}
