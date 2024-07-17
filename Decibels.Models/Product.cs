f82ec23a0e316392bcb67d3053782012a6f1dfd3
gritNgo <dustspeck00@gmail.com>
2024-07-17 15:42:59 +0200

    Create Product Model

---
+++ Decibels.Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decibels.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        [Range(1, 1000)]
+        public decimal Price { get; set; }
+    }
+}
