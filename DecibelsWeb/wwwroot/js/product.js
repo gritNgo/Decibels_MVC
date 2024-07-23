d85c49e9534032f30d5aeee5dddb4efc86319689
gritNgo <dustspeck00@gmail.com>
2024-07-23 17:25:07 +0200

    Load DataTables

---
+++ DecibelsWeb/wwwroot/js/product.js
@@ -0,0 +1,18 @@
+﻿
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/product/getall'}, // specify url in case of more properties in the future in the ajax call
+        "columns": [
+            { data: 'name', "width": "15%" },
+            { data: 'description', "width": "25%" },
+            { data: 'price', "width": "5%" },
+            { data: 'category.name', "width": "10%" }
+        ]
+    });
+}
+
