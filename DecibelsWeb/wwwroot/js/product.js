08f5e922ca93a37dc44408f1d883ffd47cd6d8be
gritNgo <dustspeck00@gmail.com>
2024-07-23 18:04:29 +0200

    Edit Product Link in DataTable

--- DecibelsWeb/wwwroot/js/product.js
+++ DecibelsWeb/wwwroot/js/product.js
@@ -9,9 +9,20 @@ function loadDataTable() {
         "ajax": { url: '/admin/product/getall'}, // specify url in case of more properties in the future in the ajax call
         "columns": [
             { data: 'name', "width": "15%" },
-            { data: 'description', "width": "25%" },
-            { data: 'price', "width": "5%" },
-            { data: 'category.name', "width": "10%" }
+            { data: 'price', "width": "10%" },
+            { data: 'category.name', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2">
+                                <i class="bi bi-pencil-square"></i> Edit</a>
+                    <a class="btn btn-danger mx-2">
+                                <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                },
+                "width": "15%"
+            }
         ]
     });
 }
