5501960b9213fa573ef797fd21d577d358a82f09
gritNgo <dustspeck00@gmail.com>
2024-07-23 18:48:38 +0200

    Sweetalerts

--- DecibelsWeb/Areas/Admin/Controllers/ProductController.cs
+++ DecibelsWeb/Areas/Admin/Controllers/ProductController.cs
@@ -126,6 +126,7 @@ namespace DecibelsWeb.Areas.Admin.Controllers
             return Json(new { data = objProductList });
         }
 
+        [HttpDelete]
         public IActionResult Delete(int? id)
         {
             var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
