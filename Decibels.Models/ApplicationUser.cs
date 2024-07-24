0b4ae82eb7ae33cb7e2c7314d51c89447a64b832
gritNgo <dustspeck00@gmail.com>
2024-07-24 11:16:11 +0200

    Extend IdentityUser

---
+++ Decibels.Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decibels.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        // Discriminator column found in database will have a value that will tell whether the User on that record is an ApplicationUser or an IdentityUser
+        public string? Street { get; set; }
+        public string? City{ get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+
+    }
+}
