ae294594ec550f9a1331125b3913655f2dfc19a4
gritNgo <dustspeck00@gmail.com>
2024-07-27 12:01:55 +0200

    Add ShoppingCart Model

--- Decibels.DataAccess/Data/ApplicationDbContext.cs
+++ Decibels.DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,7 @@ namespace Decibels.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
