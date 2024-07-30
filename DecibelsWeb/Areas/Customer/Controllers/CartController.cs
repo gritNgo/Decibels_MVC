6eba0efb526c907818b19c3c007ef06ee8d5033f
gritNgo <dustspeck00@gmail.com>
2024-07-30 07:53:15 +0200

    Shopping Cart UI

---
+++ DecibelsWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DecibelsWeb.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        [Area("customer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
