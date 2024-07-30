f846e51bf7039007dbe07647d5197e0774bbdc87
gritNgo <dustspeck00@gmail.com>
2024-07-30 11:37:55 +0200

    Create Order Header and Details Models

---
+++ Decibels.Models/OrderDetail.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decibels.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        [Required]
+        public int OrderHeaderId { get; set; }
+        [ForeignKey("OrderHeaderId")]
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+        // price set at time of adding to cart that does not change in case Product Price gets updated
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal Price { get; set; }
+    }
+}
