2d28a4b6507bfbe058f5d8a2750ead79ed4efe5e
gritNgo <dustspeck00@gmail.com>
2024-07-31 15:36:03 +0200

    Filters in Order List

--- DecibelsWeb/Areas/Admin/Controllers/OrderController.cs
+++ DecibelsWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,8 +1,10 @@
 ﻿using Decibels.DataAccess.Repository;
 using Decibels.DataAccess.Repository.IRepository;
 using Decibels.Models;
+using Decibels.Utility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace DecibelsWeb.Areas.Admin.Controllers
 {
@@ -24,9 +26,31 @@ namespace DecibelsWeb.Areas.Admin.Controllers
 
         #region API CALLS
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            // change from List to IEnumerable for the filtering to work
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == StaticDetails.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+
+
             return Json(new { data = objOrderHeaders });
         }
 
