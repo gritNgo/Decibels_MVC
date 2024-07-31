74f75c322de29dcad270b61c53d2ddec6aedf1b8
gritNgo <dustspeck00@gmail.com>
2024-07-31 12:48:31 +0200

    Orders List UI

---
+++ DecibelsWeb/wwwroot/js/order.js
@@ -0,0 +1,30 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/order/getall' }, // specify url in case of more properties in the future in the ajax call
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "15%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'applicationUser.email', "width": "10%" },
+            { data: 'orderStatus', "width": "10%" },
+            { data: 'orderTotal', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-100 btn-group" role="group">
+                    <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2">
+                                <i class="bi bi-pencil-square"></i> </a>
+                    </div>`
+                },
+                "width": "15%"
+            }
+        ]
+    });
+}
+
