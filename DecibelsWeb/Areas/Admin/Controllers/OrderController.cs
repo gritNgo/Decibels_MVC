5f5e7fad327db33f57d12db969e67dfab3dc14e2
gritNgo <dustspeck00@gmail.com>
2024-08-01 13:00:38 +0200

    Update Order Details

--- DecibelsWeb/Areas/Admin/Controllers/OrderController.cs
+++ DecibelsWeb/Areas/Admin/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using Decibels.DataAccess.Repository.IRepository;
 using Decibels.Models;
 using Decibels.Models.ViewModels;
 using Decibels.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -14,6 +15,8 @@ namespace DecibelsWeb.Areas.Admin.Controllers
     {
 
         private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
+        public OrderVM OrderVM { get; set; }
 
         public OrderController(IUnitOfWork unitOfWork)
         {
@@ -28,13 +31,42 @@ namespace DecibelsWeb.Areas.Admin.Controllers
         public IActionResult Details(int orderId)
         {
             // based on orderId populate VM
-            OrderVM orderVM = new()
+            OrderVM = new()
             {
                 OrderHeader = _unitOfWork.OrderHeader
                 .Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
                 OrderDetail = _unitOfWork.OrderDetail.GetAll(u=>u.OrderHeaderId==orderId, includeProperties:"Product")
             };
-            return View(orderVM);
+            return View(OrderVM);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = StaticDetails.Role_Admin+","+StaticDetails.Role_Employee)]
+        public IActionResult UpdateOrderDetails()
+        {
+            var orderHeaderFromDb = _unitOfWork.OrderHeader.Get(u=>u.Id==OrderVM.OrderHeader.Id); // the id passed in the hidden input field of the view
+            // map the fields to update
+            orderHeaderFromDb.Name = OrderVM.OrderHeader.Name;
+            orderHeaderFromDb.PhoneNumber = OrderVM.OrderHeader.PhoneNumber;
+            orderHeaderFromDb.Street = OrderVM.OrderHeader.Street;
+            orderHeaderFromDb.City = OrderVM.OrderHeader.City;
+            orderHeaderFromDb.State = OrderVM.OrderHeader.State;
+            orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
+
+            if (!string.IsNullOrEmpty(OrderVM.OrderHeader.Courier))
+            {
+                orderHeaderFromDb.Courier = OrderVM.OrderHeader.Courier;
+            }
+            if (!string.IsNullOrEmpty(OrderVM.OrderHeader.TrackingNumber))
+            {
+                orderHeaderFromDb.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            }
+            _unitOfWork.OrderHeader.Update(orderHeaderFromDb);
+            _unitOfWork.Save();
+
+            TempData["Success"] = "Order Details Updated Successfully.";
+
+            return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.Id}); // redirect and update Id in database
         }
 
         #region API CALLS
